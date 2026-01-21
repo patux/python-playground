4db0a112377f148333f2bac513a210c467afba68
Geronimo Orozco <188718+patux@users.noreply.github.com>
2026-01-20 18:41:29 -0600

    fib written bit better

--- fib.py
+++ fib.py
@@ -1,24 +1,13 @@
-#!/usr/bin/env python
-def Fibonacci(n):
-    a = 0 
-    b = 1
+# TODO create the fibonacci generator and find the first Fibonacci number > 100
+def fibonacci():
+  a, b = 0, 1
+  while True:
+    a, b = b, a + b
+    yield b
 
-    # if n < 0 error
-    if n < 0:
-        print ("Error")
-    # if n = 0 return 0
-    elif n == 0:
-        return 0
-    # if n = 1 return 1
-    elif n == 1:
-        print (b, end=" ")
-        return b
-    else:
-        for i in range(1, n):
-            c = a + b
-            a = b
-            b = c
-            print (b, end=" ")
-        return b
 
-print (Fibonacci(9))
+# Example usage:
+for fib in fibonacci():
+    if fib > 100:
+        print(f"The first Fibonacci number greater than 100 is: {fib}")
+        break
